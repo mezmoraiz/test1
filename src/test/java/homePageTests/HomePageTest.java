c27293955c4115c823d7867242048da2457e3686
Mezmorayz <zerokood15@gmail.com>
2026-09-20 17:38:30 +0300

    FirstTestAllo

---
+++ src/test/java/homePageTests/HomePageTest.java
@@ -0,0 +1,56 @@
+package homePageTests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static java.lang.Thread.sleep;
+
+public class HomePageTest {
+
+    @Test
+    public void checkAlloLogo() throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://allo.ua/ru/");
+
+        sleep(5000);
+
+        WebElement AlloLogo = driver.findElement(By.xpath("//a[@class='v-logo']"));
+
+        AlloLogo.isDisplayed();
+
+        Assert.assertTrue(AlloLogo.isDisplayed());
+
+        driver.quit();
+    }
+//    @Test
+//    public void clickAlloLogo() throws InterruptedException {
+//
+//        WebDriver driver = new ChromeDriver();
+//
+//        driver.manage().window().maximize();
+//
+//        driver.get("https://allo.ua/ru/");
+//
+//        WebElement AlloLogo = driver.findElement(By.xpath("//a[@class='v-logo']"));
+//
+//        sleep(5000);
+//
+//        AlloLogo.isDisplayed();
+//        AlloLogo.click();
+//
+//        sleep(2000);
+//
+//        driver.quit();
+//
+//    }
+
+
+}
