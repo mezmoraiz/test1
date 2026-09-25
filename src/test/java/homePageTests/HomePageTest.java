258f2d96dd0ace3d3d1dbd15587a32a4334dc17d
Mezmorayz <zerokood15@gmail.com>
2026-09-25 23:39:19 +0300

    Update HomePageTest

--- src/test/java/homePageTests/HomePageTest.java
+++ src/test/java/homePageTests/HomePageTest.java
@@ -25,7 +25,7 @@ public class HomePageTest {
         WebElement alloLogo = driver.findElement(By.xpath("//a[@class='v-logo']"));
 
         Assert.assertTrue(alloLogo.isDisplayed());
-пш
+
         driver.quit();
     }
 }
